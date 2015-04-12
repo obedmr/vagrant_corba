d8eeb1ba628f6ae3912749bded202180449698a6
Obed N Munoz <obed.n.munoz@gmail.com>
2015-04-11 23:20:59 -0500

    Clone CORBA Java source code
    Signed-off-by: Obed N Munoz <obed.n.munoz@gmail.com>

--- scripts/server.sh
+++ scripts/server.sh
@@ -7,16 +7,8 @@ pacman -Syu --noconfirm git
 pacman -S --noconfirm jdk8-openjdk
 
 # Clone Java CORBA Project
-#cd /vagrant
-#git clone https://github.com/VictorRodriguez/distributed_systems.git
-#chown vagrant.vagrant -R distributed_systems
-#cd distributed_systems/src
+cd /vagrant
+git clone https://github.com/VictorRodriguez/distributed_systems.git
+chown vagrant.vagrant -R distributed_systems
 
-# Compile Server
-#javac -d ./ example/hello/*.java
 
-# Starting RMI Registry
-#rmiregistry &
-
-# Starting Server
-#java -cp ./ -Djava.rmi.server.codebase=file:./ -Djava.rmi.server.hostname=10.0.0.10 example.hello.Server &
